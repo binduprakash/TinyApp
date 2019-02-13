62d24cb80ae4ee007bfcbeb68a272f2a2c1b14ee
Bindu Prakash <bindu2305@gmail.com>
2019-02-13 03:49:42 +0000

    Random short URL generated

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
+const bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -9,13 +11,32 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  var array = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".slice('');
+  var randomString = '';
+  for (var i = 0; i < 6; i++){
+    var index = Math.floor((Math.random() * 61) + 1);
+    randomString += array[index];
+  }
+  return randomString;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -25,6 +46,7 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
